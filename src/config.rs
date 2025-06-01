dfc716da5ca048a1be7c9a7531aa8a0dc8f8230d
yqhysm <58025587+yqhysm@users.noreply.github.com>
2025-06-01 14:51:08 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["22.139.123.7"];
+pub const RS_PUB_KEY: &str = "4V9dd9VPkxrTDKcm62zFCtzFtJSEhUGGV+SY1V2Goys=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
